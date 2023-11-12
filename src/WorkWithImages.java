bf497ab1cd22215c76423ed6e7cd30a851fe9807
AnithaK94 <kannananitha.94@gmail.com>
2023-11-12 22:40:57 +0530

    refactored

---
+++ src/WorkWithImages.java
@@ -0,0 +1,56 @@
+import org.checkerframework.checker.units.qual.A;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
+public class WorkWithImages {
+    public static void main(String[] args) throws InterruptedException, AWTException {
+        WebDriver driver = DriverUtils.setDriverPathAndGetWebDriver("https://www.leafground.com/file.xhtml");
+        // CLICK THE IMAGE IT GOES TO HOME PAGE
+        WebElement imageClick = driver.findElement(By.id("j_idt13"));
+        imageClick.click();
+        //BACK TO THE PAGE
+        driver.navigate().back();
+        //CHECK FOR BTOKEN IMAGE
+        WebElement imageBroke = driver.findElement(By.id("j_idt13"));
+        if(imageBroke.getAttribute("naturalWidth").equals(null)){
+            System.out.println("The image is broken");
+        }else{
+            System.out.println("The image is not Broken");
+        }
+        //UPLOAD AN IMAGE
+        WebElement uploadImageButton = driver.findElement(By.xpath("//*[@id=\'j_idt97:j_idt98\']/div[1]/span"));
+        Thread.sleep(4000);
+
+        StringSelection imagefile = new StringSelection("C:\\Users\\kanna\\OneDrive\\Pictures\\image");
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(imagefile,null);
+
+        Actions builder = new Actions(driver);
+        builder.click(uploadImageButton).release().build().perform();
+
+        Robot rbt = new Robot();
+        rbt.delay(4000);
+        rbt.keyPress(KeyEvent.VK_CONTROL);
+        rbt.keyPress(KeyEvent.VK_V);
+        rbt.keyRelease(KeyEvent.VK_V);
+        rbt.keyRelease(KeyEvent.VK_CONTROL);
+        rbt.keyPress(KeyEvent.VK_ENTER);
+        rbt.keyRelease(KeyEvent.VK_ENTER);
+        rbt.delay(4000);
+        System.out.println("The image is uploaded Successfully");
+        WebElement uploadButton = driver.findElement(By.xpath("//*[@id=\'j_idt97:j_idt98\']/div[1]/button[1]"));
+        uploadButton.click();
+        Thread.sleep(2000);
+        // CLOSE THE DRIVER
+        driver.quit();
+
+    }
+}
+// navigate().to = do get function
+
--- src/WorkWithUploadFiles.java
+++ src/WorkWithUploadFiles.java
@@ -1,43 +1,39 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 
 public class WorkWithUploadFiles {
-    public static void main(String[] args) throws AWTException {
+    public static void main(String[] args) throws AWTException, InterruptedException {
         WebDriver driver = DriverUtils.setDriverPathAndGetWebDriver("https://letcode.in/file");
 
         WebElement uploadButton = driver.findElement(By.xpath("/html/body/app-root/app-filemanagement/section[1]/div/div/div[1]/div/div[1]/div/div/label/span/span[2]"));
-        uploadButton.click();
+        Thread.sleep(4000);
        // uploadButton.sendKeys("\'C:\\Users\\kanna\\Downloads\\sample.xlsx\'");
 
-       String file = "C:\\Users\\kanna\\Downloads\\sample.xlsx" ;
-       StringSelection selectFile = new StringSelection(file); // data transferrable
-
+       //String file = "C:\\Users\\kanna\\Downloads\\sample.xlsx" ;
+       //StringSelection selectFile = new StringSelection(file); // data transferrable
         // copy contents operation in windows - java operation
-        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selectFile,null);
-        System.out.println("Getting the File Location.....");
+        StringSelection xlfile = new StringSelection("C:\\Users\\kanna\\Downloads\\sample");
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(xlfile,null);
+
+        Actions builder = new Actions(driver);
+        builder.click(uploadButton).release().build().perform();
         // paste operations - For windows actions use Robot Class
-        Robot robotAction = new Robot();
-        robotAction.keyPress(KeyEvent.VK_CONTROL);
-        robotAction.keyPress(KeyEvent.VK_V);
-        robotAction.keyRelease(KeyEvent.VK_V);
-        robotAction.keyRelease(KeyEvent.VK_CONTROL);
-        System.out.println("The File is copied.....");
-        // Enter the pasted file
-        robotAction.keyPress(KeyEvent.VK_ENTER);
-        robotAction.keyRelease(KeyEvent.VK_ENTER);
+        Robot rbt = new Robot();
+        rbt.delay(4000);
+        rbt.keyPress(KeyEvent.VK_CONTROL);
+        rbt.keyPress(KeyEvent.VK_V);
+        rbt.keyRelease(KeyEvent.VK_V);
+        rbt.keyRelease(KeyEvent.VK_CONTROL);
+        rbt.keyPress(KeyEvent.VK_ENTER);
+        rbt.keyRelease(KeyEvent.VK_ENTER);
+        rbt.delay(4000);
         System.out.println("The File is Uploaded!!!!");
-
-
-
-
-
-
-
 
     }
 }
