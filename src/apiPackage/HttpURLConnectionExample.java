ab1d8b9e13e0e44762df285ef051d4a953648303
AnithaK94 <kannananitha.94@gmail.com>
2023-11-29 22:57:48 +0530

    refactored

--- src/apiPackage/HttpURLConnectionExample.java
+++ src/apiPackage/HttpURLConnectionExample.java
@@ -2,6 +2,7 @@ package apiPackage;
 
 import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class HttpURLConnectionExample {
@@ -27,11 +28,38 @@ public class HttpURLConnectionExample {
             buffer.append(line);
         }
         System.out.println(buffer);
+    }
+    public void postMethodExample() throws IOException {
+        URL url = new URL("https://dummy.restapiexample.com/api/v1/create");
+        HttpURLConnection connectionPost = (HttpURLConnection) url.openConnection();
+        connectionPost.setRequestMethod("POST");
+        connectionPost.setRequestProperty("Content_Type","application/json");
+        connectionPost.setDoOutput(true);
+
+        String jsonBody = "{\"name\":\"Anil\",\"salary\":\"123456\",\"age\":\"2\"}";
+        byte[] inputJson = jsonBody.getBytes();
+
+        OutputStream outputStream = connectionPost.getOutputStream();
+        outputStream.write(inputJson);
+        System.out.println("Response Code : " +connectionPost.getResponseCode());
+        System.out.println("Response Message : " +connectionPost.getResponseMessage());
+
+        InputStream inputStream = connectionPost.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String msg;
+        StringBuffer bufferMsg = new StringBuffer();
+
+        while ((msg = bufferedReader.readLine())!= null){
+            bufferMsg.append(msg);
+        }
+        System.out.println(msg);
 
     }
 
     public static void main(String[] args)  throws IOException {
         HttpURLConnectionExample connectEx = new HttpURLConnectionExample();
         connectEx.getMethodExample();
+        connectEx.postMethodExample();
     }
 }
