c549d62b9d6824d3bbc7fc9c92a4ac874358dd1c
AnithaK94 <kannananitha.94@gmail.com>
2023-12-01 23:08:50 +0530

    refactored

---
+++ src/testNGFramework/SampleTestClass.java
@@ -0,0 +1,26 @@
+package testNGFramework;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+public class SampleTestClass {
+
+//    private static Logger logger = LoggerFactory.getLogger(SampleTestClass.class);
+    @Test
+    public void firstTestCase(){
+        System.out.println("This is the first TestCase");
+    }
+    @Test
+    public void secondTestCase(){
+        System.out.println("This is the second TestCase");
+    }
+    @Test
+    public void thirdTestCase(){
+        System.out.println("This is the third TestCase");
+    }
+    @Test
+    public void fourthTestCase(){
+        System.out.println("This is the fourth TestCase");
+    }
+}
