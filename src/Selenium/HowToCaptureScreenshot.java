c1fce627c5685d2a9983a5dc81ef5dfe59b7e058
AnithaK94 <kannananitha.94@gmail.com>
2023-12-18 21:37:42 +0530

    refactored

--- src/Selenium/HowToCaptureScreenshot.java
+++ src/Selenium/HowToCaptureScreenshot.java
@@ -1,7 +1,9 @@
 package Selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.io.FileHandler;
 import ru.yandex.qatools.ashot.AShot;
 import ru.yandex.qatools.ashot.Screenshot;
@@ -14,7 +16,7 @@ import java.io.IOException;
 public class HowToCaptureScreenshot {
     public static void main(String[] args) throws IOException {
         //open browser and go to the required link
-        WebDriver driver = DriverUtils.setDriverPathAndGetWebDriver("https://www.leafground.com/button.xhtml");
+        WebDriver driver = DriverUtils.setDriverPathAndGetWebDriver("https://letcode.in/buttons");
 
         // It only can take screenshot the DOM - it done mostly
 
@@ -25,7 +27,14 @@ public class HowToCaptureScreenshot {
         // to get full page screenshot
         // capture screenshot and store the image
         Screenshot s=new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver);
-        ImageIO.write(s.getImage(),"PNG",new File("./snaps/img.png"));
+        ImageIO.write(s.getImage(),"PNG",new File("./snaps/ltimg.png"));
+
+      /*  // element Screenshot
+        WebElement element = driver.findElement(By.id("home"));
+        // Along with driver pass element also in takeScreenshot() method.
+        Screenshot s1=new AShot().shootingStrategy(ShootingStrategies.viewportPasting(1000)).takeScreenshot(driver,element);
+        ImageIO.write(s1.getImage(),"PNG",new File("./snaps/lteleimg.png"));*/
+
 
         //closing the webdriver
         driver.quit();
