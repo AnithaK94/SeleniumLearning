0d9ea48082d6193e0664da065f4dccba3b36fae6
AnithaK94 <kannananitha.94@gmail.com>
2024-01-10 22:35:17 +0530

    refactored

--- src/Selenium/WorkWithTextElements.java
+++ src/Selenium/WorkWithTextElements.java
@@ -29,6 +29,35 @@ public class WorkWithTextElements {
             System.out.println(text);
         }
 
+        WebElement inputBox = driver.findElement(By.linkText("Edit"));
+        inputBox.click();
+
+        // Enter your full Name
+        WebElement nameTextBox = driver.findElement(By.id("fullName"));
+        nameTextBox.sendKeys("Anitha Kannan");
+
+        //Append a text and press keyboard tab
+        WebElement appendTextBox = driver.findElement(By.id("join"));
+        appendTextBox.sendKeys("girl"+ Keys.TAB);
+
+        //What is inside the text box
+        WebElement retriveTextBox = driver.findElement(By.id("getMe"));
+        String value = retriveTextBox.getAttribute("value");
+        System.out.println("The value inside the Retrieve Text Box is : " +value);
+
+        //Clear the text
+        WebElement clearTextBox = driver.findElement(By.id("clearMe"));
+        clearTextBox.clear();
+
+        //Confirm edit field is disabled
+        WebElement checkDisableBox = driver.findElement(By.id("noEdit"));
+        System.out.println(" Is The Edit field Enabled ? : " + checkDisableBox.isEnabled());
+
+        //Confirm text field is readonly
+        WebElement checkReadOnlyBox = driver.findElement(By.id("dontwrite"));
+        System.out.println("Is the selected TextBox is Readonly? : " + checkReadOnlyBox.getAttribute("value"));
+
+        //after the operations performed in the opened Home page, close the home page
         driver.quit();
     }
 
